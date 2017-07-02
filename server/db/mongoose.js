e3b690751d3a94b3dbe91f988a00af52b6caa56b
upendra117@gmail.com <upendra117@gmail.com>
2017-07-02 18:51:11 +0100

    Setup test and development environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 // path to the connection and collection name
-var url = (process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+var url = (process.env.MONGODB_URI);
 // mongoose default Promise
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
