95a2266a177cd2b60455f15945f79ef69c4ef844
upendra117@gmail.com <upendra117@gmail.com>
2017-07-02 14:55:56 +0100

    Add DELETE /todos/id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,7 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+
+// Todo.remove({}).then((done) => done());
+
